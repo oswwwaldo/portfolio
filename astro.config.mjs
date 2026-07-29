f1af18ba1087c77289aa099bdc38b247c30d460c
Oswaldo Fabrizio De Los Santos Ascencio <ofdelossantosa@gmail.com>
2026-07-28 20:14:18 -0400

    chore(astro.config): WIP troubleshooting cloudflare ssr errors
    Enabling SSR causes Vite to throw a "module is not defined" error.
    Suspecting a conflict between astro-icon and the cloudflare adapter.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,17 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
+import cloudflare from '@astrojs/cloudflare';
 import icon from 'astro-icon';
 
 // https://astro.build/config
+// https://docs.astro.build/en/guides/integrations-guide/cloudflare/
 export default defineConfig({
-  integrations: [icon()]
+  integrations: [icon()],
+  output: 'static', // or 'server'
+  adapter: cloudflare(),
+  vite: {
+    ssr: {
+      noExternal: ['astro-icon', 'debug'], 
+    },
+  },
 });
