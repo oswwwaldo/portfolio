c8c4bd530410dfd1fff4d84f1d6c666e6866aa3b
Oswaldo Fabrizio De Los Santos Ascencio <ofdelossantosa@gmail.com>
2026-08-10 12:59:32 -0400

    style(scripts): adjust console.log, move comments to top

--- src/scripts/get-yt-data-json.js
+++ src/scripts/get-yt-data-json.js
@@ -1,25 +1,21 @@
 
-// This is a prebuild script, which will launch before npm run build and because Vite does not handle this part of the build process then the variable must exist through Node.js's environment 
+// ? This is a prebuild script, which will launch before npm run build and because Vite does not handle this part of the build process then the variable must exist through Node.js's environment 
+// ? https://developers.google.com/youtube/v3/docs/playlistItems/list
+// ? https://developers.google.com/youtube/v3/docs/playlistItems#resource
+
 const DATA_API_KEY = process.env.YT_DATA_API_KEY;
+
 const CHANNEL_ID = `UCAa9zq-rl0McMewzW9tra0w`;
 const playlistIDs = [`PLfPCDW7xyAww`, `PLUNtUmhxA3zm779NGq3Na99MX7B8L3X3K`, `PLUNtUmhxA3zlJJrCJmcYWHGB9g4lP0GW5`, `PLUNtUmhxA3zmaC2NgXLoEFsPJ5B9fTVHP`, `PLUNtUmhxA3zm_N4Kg53VY9TkHgtaeImsb`, `PLUNtUmhxA3znFD2M0yCqVw4wyozMco0Xo`];
 
 console.log(" ");
 console.log("\x1b[32m%s\x1b[0m", "Success: Script loaded!");
-// console.log("\x1b[31m%s\x1b[0m", "Error: Process failed!");
-// console.log("\x1b[33m\x1b[44m%s\x1b[0m", "Warning: Check parameters!");
 
 const now = new Date();
 console.log("Running get-yt-data-json.js — " + now.toLocaleTimeString());
 
-
-// https://developers.google.com/youtube/v3/docs/playlistItems/list
-// https://developers.google.com/youtube/v3/docs/playlistItems#resource
-
 export async function buildYTData() {
-
     try {
-
         const musicDatabase = {};
 
         const fetchPromises = playlistIDs.map(async (id, index) => {
@@ -32,6 +28,8 @@ export async function buildYTData() {
         
         const playlistsResults = await Promise.allSettled(fetchPromises);
         console.log("Grabbed: " + playlistsResults.length + " playlists");
+        console.log(await playlistsResults[0].value);
+
         playlistsResults.forEach((result) => {
             if (result.status === 'rejected') {
                 console.log("\x1b[31m%s\x1b[0m", `Error: a playlist fetch failed — ${result.reason.message}`);
@@ -42,8 +40,9 @@ export async function buildYTData() {
 
             let playlistContents = [];
 
+            console.log(playlist.data.items);
             playlist.data.items.forEach((item) => {
-                if (item.snippet.title && item.snippet.thumbnails &&    item.snippet.title != "Deleted video" && item.snippet.title != "Private video"){
+                if (item.snippet.title && item.snippet.thumbnails && item.snippet.title != "Deleted video" && item.snippet.title != "Private video"){
                     playlistContents.push({
                         videoPos: item.snippet?.position,
                         videoId: item.contentDetails?.videoId,
@@ -68,7 +67,4 @@ export async function buildYTData() {
     }
 }
 
-// https://www.youtube.com/watch?v=${videoId}
-// await buildYTData();
-// console.log(await buildYTData());
 export const message = "Hello from get-yt-data.json.js";
