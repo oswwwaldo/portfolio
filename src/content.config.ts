87c70b29a0a6d019e75d23ec0ab638a7c9a5e05a
Oswaldo Fabrizio De Los Santos Ascencio <ofdelossantosa@gmail.com>
2026-08-10 11:06:52 -0400

    feat(content): add experience, thoughts zod schemas and export

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,11 +1,37 @@
-import { file } from "astro/loaders"
+import { file, glob } from "astro/loaders"
 import { defineCollection } from "astro:content"
 import { z } from "astro/zod"
 
-const projects = defineCollection({
-    loader: file('src/data/projects.json'),
+const experience = defineCollection({
+    loader: file('src/data/experience.json'),
     schema: z.object({
-        id: z.string(),
+        title: z.string(),
+        caption: z.string(),
+        description: z.string(),
+        timeline: z.string(),
+        skills: z.array(z.string()),
+        format: z.string(),
+    })
+})
+
+const thoughts = defineCollection({
+    loader: glob({ base: 'src/content/thoughts', pattern: '*.md'}),
+    schema: z.object({
+        title: z.string(),
+        description: z.string(),
+        pubDate: z.coerce.date(),
+        updatedDate: z.coerce.date().nullable().optional(),
+        thumbnail: z.string(),
+        tags: z.array(z.string()),
+        featured: z.boolean().default(false),
+        draft: z.boolean().default(false),
+    }),
+})
+
+const projects = defineCollection({
+    // loader: file('src/data/projects.json'),
+    loader: glob({ base: 'src/content/projects', pattern: '*.md'}),
+    schema: z.object({
         title: z.string(),
         slug: z.string(),
         tagline: z.string(),
@@ -33,4 +59,4 @@ const projects = defineCollection({
     })
 })
 
-export const collections = { projects }
+export const collections = { experience, projects, thoughts }
