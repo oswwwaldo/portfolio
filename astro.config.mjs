9ee061ec8f7fbcb212ac9267da04de338b18ad8a
Oswaldo Fabrizio De Los Santos Ascencio <ofdelossantosa@gmail.com>
2026-08-16 20:34:34 -0400

    feat(config): import @astrojs/sitemap, and dynamically return site URL

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,26 @@
 // @ts-check
 import { defineConfig, envField } from 'astro/config';
-
 import icon from 'astro-icon';
+import sitemap from "@astrojs/sitemap";
+
+const getSiteUrl = () => {
+    if (process.env.CF_PAGES_BRANCH === 'main') {
+        return 'http://portfolio.ofdelossantosa.workers.dev/';
+    }
+    if (process.env.CF_PAGES_URL) {
+        return process.env.CF_PAGES_URL; 
+    }
+        
+    return 'http://localhost:4321'; 
+};
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [icon()],
-  env: {
-    schema: {
-      YT_DATA_API_KEY: envField.string({ context: "server", access: "secret" }),
+    site: getSiteUrl(),
+    integrations: [icon(), sitemap()],
+    env: {
+        schema: {
+            YT_DATA_API_KEY: envField.string({ context: "server", access: "secret" }),
+        },
     },
-  },
 });
